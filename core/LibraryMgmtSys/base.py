b818b646fa517678c733f44b06d02e546d72830f
SaugatMgr <saugatmagar433@gmail.com>
2024-02-29 14:11:17 +0545

    corrected format of AUTH_PASSWORD_VALIDATORS

--- core/LibraryMgmtSys/base.py
+++ core/LibraryMgmtSys/base.py
@@ -68,14 +68,18 @@ WSGI_APPLICATION = "core.LibraryMgmtSys.wsgi.application"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": (
-            "django.contrib.auth.password_validation."
-            "UserAttributeSimilarityValidator",
-        )
+        "NAME": "django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator",
     },
-    {"NAME": ("django.contrib.auth.password_validation." "MinimumLengthValidator",)},
-    {"NAME": ("django.contrib.auth.password_validation." "CommonPasswordValidator",)},
-    {"NAME": ("django.contrib.auth.password_validation." "NumericPasswordValidator",)},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
