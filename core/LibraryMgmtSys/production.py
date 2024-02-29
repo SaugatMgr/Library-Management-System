153c0e1c0ec5fb2ad9a504e1c5da2e42ab46c0d0
SaugatMgr <saugatmagar433@gmail.com>
2024-02-29 13:18:23 +0545

    ruff code auto format

--- core/LibraryMgmtSys/production.py
+++ core/LibraryMgmtSys/production.py
@@ -3,9 +3,9 @@ import environ
 from .base import *
 
 env = environ.Env()
-environ.Env.read_env('core/LibraryMgmtSys/.env.production')
+environ.Env.read_env("core/LibraryMgmtSys/.env.production")
 
-DEBUG = env.bool('DEBUG', default=False)
-SECRET_KEY = env.str('SECRET_KEY')
-DATABASES = {'default': env.db('DATABASE_URL')}
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+DEBUG = env.bool("DEBUG", default=False)
+SECRET_KEY = env.str("SECRET_KEY")
+DATABASES = {"default": env.db("DATABASE_URL")}
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
