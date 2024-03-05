9073c8c1b782f2ba97df1f31e184f3f6ced23cc9
SaugatMgr <saugatmagar433@gmail.com>
2024-03-05 13:33:47 +0545

    added baserouter urls and user app urls

--- core/LibraryMgmtSys/urls.py
+++ core/LibraryMgmtSys/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from .routers import base_router
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
+    path("api/v1/", include(base_router.urls)),
+    path("api/v1/", include("apps.users.api.v1.urls.urls")),
 ]
