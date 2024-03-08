90632fe775ac844e3d48da3a0a83e271e067a7be
SaugatMgr <saugatmagar433@gmail.com>
2024-03-08 23:56:13 +0545

    user profile registered

--- apps/users/api/v1/urls/routes.py
+++ apps/users/api/v1/urls/routes.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from apps.users.api.v1.views.viewsets import UserViewset
+from apps.users.api.v1.views.viewsets import UserProfileViewSet, UserViewset
 
 user_router = DefaultRouter()
 
 user_router.register("user", UserViewset, basename="user")
+user_router.register("user-profile", UserProfileViewSet, basename="user-profile")
