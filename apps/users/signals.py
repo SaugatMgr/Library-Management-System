e1970a559e7208ff1994de7571b1f501c48d4172
SaugatMgr <saugatmagar433@gmail.com>
2024-03-08 23:53:07 +0545

    signal to create or update user profile based on save method call on custom user instance

---
+++ apps/users/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from apps.users.api.v1.helpers import generate_lib_card_no
+
+from apps.users.models import CustomUser, UserProfile
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    library_card_number = generate_lib_card_no()
+    data = {"user": instance, "library_card_number": library_card_number}
+    if created:
+        UserProfile.objects.create(**data)
+    else:
+        if not UserProfile.objects.filter(user=instance).exists():
+            UserProfile.objects.create(**data)
+        else:
+            instance.userprofile.save()
