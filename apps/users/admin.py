5a77089f4605e80c36e6adc32b1521ae72fa42e9
SaugatMgr <saugatmagar433@gmail.com>
2024-03-08 23:59:36 +0545

    registered user profile model to admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import CustomUser, CustomUserGroup
+from .models import CustomUser, CustomUserGroup, UserProfile
 
 admin.site.register(CustomUser)
 admin.site.register(CustomUserGroup)
+admin.site.register(UserProfile)
