ba23afd77a1189b618e961ae2711461db6dd8fc4
SaugatMgr <saugatmagar433@gmail.com>
2024-03-08 23:59:05 +0545

    User profile repository

--- apps/users/api/v1/repository.py
+++ apps/users/api/v1/repository.py
@@ -1,6 +1,23 @@
-from apps.users.models import CustomUser
+from apps.users.api.v1.serializers.post import UserProfileUpdateSerializer
+from apps.users.models import CustomUser, UserProfile
+from utils.helpers import to_internal_value
 
 
 class UserRepository:
-    def get_all():
+    @classmethod
+    def get_all(cls):
         return CustomUser.objects.filter()
+
+
+class UserProfileRepository:
+    @classmethod
+    def get_all(cls):
+        return UserProfile.objects.filter()
+
+    @classmethod
+    def update_profile(cls, instance, data):
+        if data.get("profile_picture"):
+            data["profile_picture"] = to_internal_value(data["profile_picture"])
+        profile_serializer = UserProfileUpdateSerializer(instance=instance, data=data)
+        profile_serializer.is_valid(raise_exception=True)
+        profile_serializer.save()
