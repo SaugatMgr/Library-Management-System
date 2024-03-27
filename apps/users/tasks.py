ec54e375dd45886f76d7db2b110cc13f709d56f9
SaugatMgr <saugatmagar433@gmail.com>
2024-03-27 22:36:53 +0545

    created send mail to user shared task

---
+++ apps/users/tasks.py
@@ -0,0 +1,18 @@
+from celery import shared_task
+
+from django.core.mail import send_mail
+from django.contrib.auth import get_user_model
+
+from core.LibraryMgmtSys.local import EMAIL_HOST_USER
+
+User = get_user_model()
+
+
+@shared_task
+def send_mail_to_user(subject, message):
+    from_email = EMAIL_HOST_USER
+
+    # Get all users
+    users = User.objects.filter()
+    reciepent_list = [user.email for user in users]
+    send_mail(subject, message, from_email, reciepent_list, fail_silently=False)
