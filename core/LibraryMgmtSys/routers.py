f97766a62002df3379e05dffdae97ad100279221
SaugatMgr <saugatmagar433@gmail.com>
2024-03-27 22:33:49 +0545

    extended book app router to base router registry

--- core/LibraryMgmtSys/routers.py
+++ core/LibraryMgmtSys/routers.py
@@ -1,8 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
 from apps.users.api.v1.urls.routes import user_router
+from apps.books.api.v1.urls.routes import book_router
 
 # All apps router here
 base_router = DefaultRouter()
 
 base_router.registry.extend(user_router.registry)
+base_router.registry.extend(book_router.registry)
