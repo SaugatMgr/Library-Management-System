414381e97d4f4d906272f39713a88e8b51de4479
SaugatMgr <saugatmagar433@gmail.com>
2024-04-01 16:21:32 +0545

    registered book app models

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -1,3 +1,10 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from .models import Reserve, Tag, Genre, Book, Borrow
+
+admin.site.register(Tag)
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register(Borrow)
+admin.site.register(Reserve)
