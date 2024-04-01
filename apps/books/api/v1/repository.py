6c48100f78a2880d5e408705c4ec44ec536ec001
SaugatMgr <saugatmagar433@gmail.com>
2024-04-01 16:35:55 +0545

    added reserve repository to store db business logic of reserve model; added logic for reserving book, returning book and modified borrow book logic

--- apps/books/api/v1/repository.py
+++ apps/books/api/v1/repository.py
@@ -1,10 +1,18 @@
 from datetime import timedelta
 from django.utils import timezone
-from rest_framework.exceptions import NotAcceptable
 
-from apps.books.models import Book, Borrow
-from utils.constants import BookStatusChoices
+from rest_framework.exceptions import NotAcceptable
+from rest_framework.response import Response
+
+from apps.books.helpers.error_messages import (
+    ALREADY_BORROWED,
+    ALREADY_RESERVED,
+    OUT_OF_STOCK,
+)
+from apps.books.models import Book, Borrow, Reserve
+from utils.constants import BookStatusChoices, BorrowStatusChoices, ReserveStatusChoices
 from utils.helpers import generate_error, get_instance_by_attr
+from utils.threads import get_current_user
 
 
 class BookRepository:
@@ -15,12 +23,19 @@ class BookRepository:
     @classmethod
     def borrow_book(cls, data):
         book = get_instance_by_attr(Book, "id", data["book_id"])
-        if book.quantity == 0:
-            raise NotAcceptable(
-                generate_error(
-                    message="Book is out of stock.", code="book_out_of_stock"
-                )
+        borrower = data["borrower"]
+        if (
+            BorrowRepository.get_all()
+            .filter(
+                book=book,
+                borrower=borrower,
+                borrow_status=BorrowStatusChoices.NOT_RETURNED,
             )
+            .exists()
+        ):
+            raise NotAcceptable(ALREADY_BORROWED)
+        if book.quantity == 0:
+            raise NotAcceptable(OUT_OF_STOCK)
         if book.availability_status == BookStatusChoices.UNAVAILABLE:
             raise NotAcceptable(
                 generate_error(
@@ -29,7 +44,7 @@ class BookRepository:
             )
         borrow_data = {
             "book_id": book.id,
-            "borrower_id": data["borrower"].id,
+            "borrower_id": borrower.id,
             "due_date": data["days"],
         }
         BorrowRepository.create(borrow_data)
@@ -39,23 +54,94 @@ class BookRepository:
             book.availability_status = BookStatusChoices.UNAVAILABLE
             book.save()
 
+    @classmethod
+    def return_book(cls, borrow_id):
+        borrow = get_instance_by_attr(Borrow, "id", borrow_id)
+        if borrow.borrow_status == BorrowStatusChoices.NOT_RETURNED:
+            book = borrow.book
+            book.quantity += 1
+            book.save()
+            if book.availability_status == BookStatusChoices.UNAVAILABLE:
+                book.availability_status = BookStatusChoices.AVAILABLE
+                book.save()
+            borrow.borrow_status = BorrowStatusChoices.RETURNED
+            borrow.returned_date = timezone.now()
+            borrow.save()
+            return True
+        else:
+            return False
+
+    @classmethod
+    def reserve_book(cls, data):
+        book = get_instance_by_attr(Book, "id", data["book_id"])
+        is_borrowed = Borrow.objects.filter(
+            book=book, borrow_status=BorrowStatusChoices.NOT_RETURNED
+        ).exists()
+        is_borrowed_by_current_user = (
+            BorrowRepository.get_all()
+            .filter(
+                book=book,
+                borrow_status=BorrowStatusChoices.NOT_RETURNED,
+            )
+            .exists()
+        )
+        is_reserved_by_current_user = (
+            ReserveRepository.get_all()
+            .filter(book=book, reserve_status=ReserveStatusChoices.PENDING)
+            .exists()
+        )
+        quantity = book.quantity
+        if quantity == 0 and not is_borrowed:
+            raise NotAcceptable(OUT_OF_STOCK)
+        if quantity > 0:
+            return Response({"message": "Book is available for borrowing."})
+        if is_borrowed_by_current_user:
+            raise NotAcceptable(ALREADY_BORROWED)
+        if is_reserved_by_current_user:
+            raise NotAcceptable(ALREADY_RESERVED)
+
+        reserve_data = {
+            "book_id": book.id,
+            "reserver_id": data["user"].id,
+        }
+        ReserveRepository.create(reserve_data)
+        return Response({"message": "Book reserved successfully."})
+
 
 class BorrowRepository:
     @classmethod
     def get_all(cls):
-        return Borrow.objects.filter()
+        return Borrow.objects.filter(borrower=get_current_user())
 
     @classmethod
     def create(cls, data):
-        if Borrow.objects.filter(
-            book_id=data["book_id"], borrower_id=data["borrower_id"]
-        ).exists():
-            raise NotAcceptable(
-                generate_error(
-                    message="You have already borrowed this book.",
-                    code="already_borrowed",
-                )
-            )
         due_date = timezone.now() + timedelta(days=int(data.pop("due_date")))
         data["due_date"] = due_date
         Borrow.objects.create(**data)
+
+
+class ReserveRepository:
+    @classmethod
+    def get_all(cls):
+        return Reserve.objects.filter(reserver=get_current_user())
+
+    @classmethod
+    def create(cls, data):
+        Reserve.objects.create(**data)
+
+    @classmethod
+    def update_reservation_status(cls, reserve_id, reserve_status, reason):
+        reserve = get_instance_by_attr(Reserve, "id", reserve_id)
+        if (
+            reserve_status == ReserveStatusChoices.REJECTED
+            or reserve_status == ReserveStatusChoices.CANCELLED
+            and not reason
+        ):
+            return Response(
+                {
+                    "error": "Please provide a reason for rejecting or cancelling the reservation."
+                },
+                status=400,
+            )
+        reserve.reserve_status = reserve_status
+        reserve.save()
