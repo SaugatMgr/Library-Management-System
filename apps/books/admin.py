fc38613c6ef21e5127d8b7aca0de29e7315a92ab
SaugatMgr <saugatmagar433@gmail.com>
2024-08-18 13:17:58 +0545

    added RatingInline to Book model to associate book related ratings for respective instances

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Reserve, ReserveQueue, Tag, Genre, Book, Borrow
+from .models import Rating, Reserve, ReserveQueue, Tag, Genre, Book, Borrow
+
+
+class RatingInline(admin.TabularInline):
+    model = Rating
+    extra = 1
+
+
+class BookAdmin(admin.ModelAdmin):
+    inlines = [RatingInline]
+
 
 admin.site.register(Tag)
 admin.site.register(Genre)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Borrow)
 admin.site.register(Reserve)
 admin.site.register(ReserveQueue)
