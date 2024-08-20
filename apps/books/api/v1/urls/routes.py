59ec487730009d202432f12fde4a126cf5a44f6d
SaugatMgr <saugatmagar433@gmail.com>
2024-08-20 14:03:16 +0545

    removed Tag route since Tag model is removed

--- apps/books/api/v1/urls/routes.py
+++ apps/books/api/v1/urls/routes.py
@@ -5,14 +5,12 @@ from apps.books.api.v1.views.viewsets import (
     GenreModelViewSet,
     RatingModelViewSet,
     ReserveModelViewSet,
-    TagModelViewSet,
     BorrowModelViewSet,
 )
 
 book_router = DefaultRouter()
 
 book_router.register("genres", GenreModelViewSet, basename="genres")
-book_router.register("tags", TagModelViewSet, basename="tags")
 book_router.register("books", BookModelViewSet, basename="books")
 book_router.register("borrow", BorrowModelViewSet, basename="borrow")
 book_router.register("reserve", ReserveModelViewSet, basename="reserve")
