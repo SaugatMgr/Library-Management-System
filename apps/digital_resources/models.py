e7f3eda5c5b4ea8178b30c9110dab9a12485d705
SaugatMgr <saugatmagar433@gmail.com>
2024-09-02 19:36:12 +0545

    clean method to check resource type matches with uploaded file type

--- apps/digital_resources/models.py
+++ apps/digital_resources/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from utils.constants import DigitalResourceTypeChoices
+from utils.helpers import compare_resource_type_with_uploaded_file
 from utils.models import CommonInfo
 
 
@@ -14,6 +16,18 @@ class DigitalResource(CommonInfo):
     related_books = models.ManyToManyField(
         "books.Book", related_name="digital_resources", blank=True
     )
+
+    def clean(self):
+        file_name = self.file.name
+        resource_type = self.resource_type
+
+        mime_type, resource_type_file_match = compare_resource_type_with_uploaded_file(
+            file_name, resource_type
+        )
+        if not resource_type_file_match:
+            raise ValidationError(
+                f"The uploaded file type ({mime_type}) does not match the selected resource type ({resource_type})."
+            )
 
     def __str__(self) -> str:
         return self.title
