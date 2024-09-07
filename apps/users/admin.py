32b6d573f720b796f35907e557de187f8a60aa97
SaugatMgr <saugatmagar433@gmail.com>
2024-09-07 11:33:55 +0545

    added OTP model to admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import CustomUser, CustomUserGroup, UserProfile
+from .models import CustomUser, CustomUserGroup, UserProfile, OTP
 
 admin.site.register(CustomUser)
 admin.site.register(CustomUserGroup)
 admin.site.register(UserProfile)
+admin.site.register(OTP)
