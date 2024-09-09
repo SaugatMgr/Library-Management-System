bc1f0976937bd8787041e1858fe99c394dda1119
SaugatMgr <saugatmagar433@gmail.com>
2024-09-09 21:17:22 +0545

    teacher router

--- apps/academic/api/v1/urls/routes.py
+++ apps/academic/api/v1/urls/routes.py
@@ -3,9 +3,11 @@ from rest_framework.routers import DefaultRouter
 from apps.academic.api.v1.views.viewsets import (
     DepartmentViewSet,
     GradeViewSet,
+    TeacherViewSet,
 )
 
 academics_router = DefaultRouter()
 
 academics_router.register("grades", GradeViewSet, basename="grades")
 academics_router.register("departments", DepartmentViewSet, basename="departments")
+academics_router.register("teachers", TeacherViewSet, basename="teachers")
