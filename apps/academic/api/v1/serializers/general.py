45a12a6341e12e12ae91f931be2a32f9333c2b58
SaugatMgr <saugatmagar433@gmail.com>
2024-09-09 21:17:05 +0545

    id field added to all serializers; added teacher serializer for teacher viewset

--- apps/academic/api/v1/serializers/general.py
+++ apps/academic/api/v1/serializers/general.py
@@ -1,21 +1,39 @@
 from rest_framework import serializers
 
-from apps.academic.models import Department, Grade
+from apps.academic.models import Department, Grade, Staff, Teacher
 
 
 class GradeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Grade
-        fields = "name"
+        fields = (
+            "id",
+            "name",
+        )
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = (
+            "id",
             "head_of_department",
             "description",
             "phone_number",
             "location",
+            "borrowing_period_days",
+        )
+
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = (
+            "id",
+            "user",
+            "employee_id",
+            "grade",
+            "department",
+            "designation",
             "borrowing_period_days",
         )
