db87a38cb91074c395924e18e6531d3b4f1bbb03
SaugatMgr <saugatmagar433@gmail.com>
2024-09-09 22:23:18 +0545

    added library sections, shelves and staffs router

--- apps/academic/api/v1/urls/routes.py
+++ apps/academic/api/v1/urls/routes.py
@@ -3,11 +3,19 @@ from rest_framework.routers import DefaultRouter
 from apps.academic.api.v1.views.viewsets import (
     DepartmentViewSet,
     GradeViewSet,
+    LibrarySectionViewSet,
+    ShelfViewSet,
     TeacherViewSet,
+    StaffViewSet,
 )
 
 academics_router = DefaultRouter()
 
 academics_router.register("grades", GradeViewSet, basename="grades")
 academics_router.register("departments", DepartmentViewSet, basename="departments")
+academics_router.register(
+    "library-sections", LibrarySectionViewSet, basename="library-sections"
+)
+academics_router.register("shelves", ShelfViewSet, basename="shelves")
 academics_router.register("teachers", TeacherViewSet, basename="teachers")
+academics_router.register("staffs", StaffViewSet, basename="staffs")
