0822b9b5137c1d80fd8e0780a0ae618f2512dbf5
SaugatMgr <saugatmagar433@gmail.com>
2024-09-09 22:24:13 +0545

    added library section, shelf and staff viewsets

--- apps/academic/api/v1/views/viewsets.py
+++ apps/academic/api/v1/views/viewsets.py
@@ -3,12 +3,18 @@ from rest_framework.viewsets import ModelViewSet
 from apps.academic.api.v1.serializers.general import (
     DepartmentSerializer,
     GradeSerializer,
+    LibrarySectionSerializer,
+    ShelfSerializer,
+    StaffSerializer,
     TeacherSerializer,
 )
 from apps.academic.models import (
     Department,
     Grade,
+    LibrarySection,
+    Shelf,
     Teacher,
+    Staff,
 )
 
 
@@ -22,6 +28,21 @@ class DepartmentViewSet(ModelViewSet):
     serializer_class = DepartmentSerializer
 
 
+class LibrarySectionViewSet(ModelViewSet):
+    queryset = LibrarySection.objects.all()
+    serializer_class = LibrarySectionSerializer
+
+
+class ShelfViewSet(ModelViewSet):
+    queryset = Shelf.objects.all()
+    serializer_class = ShelfSerializer
+
+
 class TeacherViewSet(ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
+
+
+class StaffViewSet(ModelViewSet):
+    queryset = Staff.objects.all()
+    serializer_class = StaffSerializer
