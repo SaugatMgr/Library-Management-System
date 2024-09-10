5333d5cacb00288d3aa61722fa585ab44861226d
SaugatMgr <saugatmagar433@gmail.com>
2024-09-10 11:24:35 +0545

    reference to membership app as it is inside app directory

--- apps/membership/apps.py
+++ apps/membership/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MembershipConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'membership'
+    name = 'apps.membership'
