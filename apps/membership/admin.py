349b02308b6a782edc0414215a935ea774e3fc5e
SaugatMgr <saugatmagar433@gmail.com>
2024-09-10 19:00:26 +0545

    admin configuration for membership app

--- apps/membership/admin.py
+++ apps/membership/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import MembershipPlan, Membership, Payment
 
-# Register your models here.
+
+class MembershipPlanAdmin(admin.ModelAdmin):
+    list_display = ("name", "plan_type", "price", "duration_in_days")
+    search_fields = ("name",)
+    list_filter = ("plan_type",)
+
+
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ("user", "plan", "start_date", "end_date")
+    search_fields = ("user__first_name", "user__last_name", "plan__name")
+    list_filter = ("plan", "start_date", "end_date")
+    readonly_fields = ("start_date", "end_date")
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "plan",
+        "amount",
+        "payment_method",
+        "transaction_id",
+        "status",
+    )
+    search_fields = (
+        "user__first_name",
+        "user__last_name",
+        "plan__name",
+        "transaction_id",
+    )
+    list_filter = ("payment_method", "status", "plan")
+
+
+admin.site.register(MembershipPlan, MembershipPlanAdmin)
+admin.site.register(Membership, MembershipAdmin)
+admin.site.register(Payment, PaymentAdmin)
