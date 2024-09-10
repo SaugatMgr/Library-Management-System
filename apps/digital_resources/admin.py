2e655a3f3c7a5f2fb46f9528f4e97bbb714cd9af
SaugatMgr <saugatmagar433@gmail.com>
2024-09-10 18:56:48 +0545

    admin interface configuration for digital resources app

--- apps/digital_resources/admin.py
+++ apps/digital_resources/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from .models import DigitalResource
 
-from apps.digital_resources.models import DigitalResource
 
-admin.site.register(DigitalResource)
+class DigitalResourceAdmin(admin.ModelAdmin):
+    list_display = ("title", "resource_type", "description")
+    search_fields = ("title",)
+    filter_horizontal = ("related_books",)
+    list_filter = ("resource_type",)
+
+
+admin.site.register(DigitalResource, DigitalResourceAdmin)
