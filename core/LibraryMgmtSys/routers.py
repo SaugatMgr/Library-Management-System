fdcc31f57e19d82c4e3fa064103c2692ae3d70ca
SaugatMgr <saugatmagar433@gmail.com>
2024-09-10 11:28:51 +0545

    academics app router added to the base router registry

--- core/LibraryMgmtSys/routers.py
+++ core/LibraryMgmtSys/routers.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from apps.users.api.v1.urls.routes import user_router
 from apps.books.api.v1.urls.routes import book_router
 from apps.digital_resources.api.v1.urls.routes import digital_resources_router
+from apps.academic.api.v1.urls.routes import academics_router
 
 # All apps router here
 base_router = DefaultRouter()
@@ -10,3 +11,4 @@ base_router = DefaultRouter()
 base_router.registry.extend(user_router.registry)
 base_router.registry.extend(book_router.registry)
 base_router.registry.extend(digital_resources_router.registry)
+base_router.registry.extend(academics_router.registry)
