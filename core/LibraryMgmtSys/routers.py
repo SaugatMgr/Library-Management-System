e3219b45f14d4e6faeff28a84064678ef5869a88
SaugatMgr <saugatmagar433@gmail.com>
2024-09-10 14:01:15 +0545

    added membership app router registry to base router

--- core/LibraryMgmtSys/routers.py
+++ core/LibraryMgmtSys/routers.py
@@ -4,6 +4,7 @@ from apps.users.api.v1.urls.routes import user_router
 from apps.books.api.v1.urls.routes import book_router
 from apps.digital_resources.api.v1.urls.routes import digital_resources_router
 from apps.academic.api.v1.urls.routes import academics_router
+from apps.membership.api.v1.urls.routes import membership_router
 
 # All apps router here
 base_router = DefaultRouter()
@@ -12,3 +13,4 @@ base_router.registry.extend(user_router.registry)
 base_router.registry.extend(book_router.registry)
 base_router.registry.extend(digital_resources_router.registry)
 base_router.registry.extend(academics_router.registry)
+base_router.registry.extend(membership_router.registry)
