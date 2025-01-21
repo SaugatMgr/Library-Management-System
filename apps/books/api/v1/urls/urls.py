0fcf1437cca5034dc612c5461837df52a49536d4
SaugatMgr <saugatmagar433@gmail.com>
2025-01-21 14:45:14 +0545

    export data view; added the view to urls

--- apps/books/api/v1/urls/urls.py
+++ apps/books/api/v1/urls/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from apps.books.api.v1.views.admin_dashboard import AdminDashboard
+from apps.books.api.v1.views.report import ExportDataView
 
 urlpatterns = [
     path("dashboard/admin/", AdminDashboard.as_view(), name="admin-dashboard"),
+    path("export/", ExportDataView.as_view(), name="export-data"),
 ]
---
+++ apps/books/api/v1/views/report.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+from io import BytesIO
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.http import HttpResponse
+
+from apps.books.api.v1.serializers.get import ExportDataSerializer
+from apps.books.models import Book
+
+
+class ExportDataView(APIView):
+    def post(self, request):
+        serializer = ExportDataSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        count = serializer.validated_data.get("count")
+        file_format = serializer.validated_data["format"]
+        fields = [
+            "title",
+            "author",
+            "publisher",
+            "publication_date",
+            "description",
+            "cover",
+            "pages",
+            "price",
+            "quantity",
+            "isbn",
+            "availability_status",
+        ]
+        queryset = Book.objects.all()[:count] if count else Book.objects.all()
+        data = queryset.values(*fields)
+
+        df = pd.DataFrame(data)
+        df["publication_date"] = df["publication_date"].dt.tz_localize(None)
+
+        if file_format == "csv":
+            return self.generate_csv(df, fields)
+        elif file_format == "excel":
+            return self.generate_excel(df)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def generate_csv(self, data, fields):
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = 'attachment; filename="export.csv"'
+        data.to_csv(response, columns=fields, index=False, encoding="utf-8")
+        return response
+
+    def generate_excel(self, data):
+        response = HttpResponse(
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+        response["Content-Disposition"] = 'attachment; filename="export.xlsx"'
+        with BytesIO() as buffer:
+            with pd.ExcelWriter(buffer, engine="openpyxl") as writer:
+                data.to_excel(writer, index=False)
+            buffer.seek(0)
+            response.write(buffer.read())
+        return response
