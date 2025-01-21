d29bd2aa6e1c5dc68d5cfadacfdafa7a0a30ea48
SaugatMgr <saugatmagar433@gmail.com>
2025-01-21 14:46:29 +0545

    set django settings module to local

--- core/LibraryMgmtSys/celery.py
+++ core/LibraryMgmtSys/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.LibraryMgmtSys.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.LibraryMgmtSys.local")
 
 app = Celery("LibraryMgmtSys")
 
