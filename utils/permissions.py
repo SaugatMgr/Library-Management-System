a78bdb61fa0c8e8be4b55d6c2dc7ec7c58b095b3
SaugatMgr <saugatmagar433@gmail.com>
2025-01-21 14:37:12 +0545

    corrected existing permission class; added permission for notification model

--- utils/permissions.py
+++ utils/permissions.py
@@ -17,11 +17,12 @@ class LibrarianOrAdminPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         user = request.user
+        user_group = user.groups.all().values_list("name", flat=True)
+        is_user_admin = UserGroupChoices.ADMIN in user_group
+        is_user_librarian = UserGroupChoices.LIBRARIAN in user_group
         if user:
             return user.is_active and (
-                UserGroupChoices.ADMIN
-                in user.groups.all().values_list("name", flat=True)
-                or user.is_superuser
+                is_user_admin or is_user_librarian or user.is_superuser
             )
 
 
@@ -37,4 +38,28 @@ class ProfileOwnerOrAdminPermission(permissions.BasePermission):
                 user.is_active
                 and user.is_authenticated
                 and (user.is_superuser or obj.user == request.user)
+            )
+
+
+class NotificationUserOrLibrarianOrAdminPermission(permissions.BasePermission):
+    message = generate_error(
+        message="Only Notification Owner or Librarian or Admin can access.",
+        code="owner_or_librarian_or_admin_only",
+    )
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        user_group = user.groups.all().values_list("name", flat=True)
+        is_user_admin = UserGroupChoices.ADMIN in user_group
+        is_user_librarian = UserGroupChoices.LIBRARIAN in user_group
+        if user:
+            return (
+                user.is_active
+                and user.is_authenticated
+                and (
+                    user.is_superuser
+                    or obj.user == request.user
+                    or is_user_admin
+                    or is_user_librarian
+                )
             )
