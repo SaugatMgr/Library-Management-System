fd55ab37b33ef573182f1aef08bb298b293d1ebf
SaugatMgr <saugatmagar433@gmail.com>
2025-01-25 14:06:20 +0545

    student dashboard url

--- apps/books/api/v1/urls/urls.py
+++ apps/books/api/v1/urls/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from apps.books.api.v1.views.admin_dashboard import AdminDashboard
+from apps.books.api.v1.views.dashboard.admin_dashboard import AdminDashboard
+from apps.books.api.v1.views.dashboard.student import StudentDashboard
 from apps.books.api.v1.views.report import ExportDataView
 
 urlpatterns = [
     path("dashboard/admin/", AdminDashboard.as_view(), name="admin-dashboard"),
+    path("dashboard/student/", StudentDashboard.as_view(), name="student-dashboard"),
     path("export/", ExportDataView.as_view(), name="export-data"),
 ]
