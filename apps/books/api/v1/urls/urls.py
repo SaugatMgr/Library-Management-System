89c84637bc51583b87f60aa765943482874268a5
SaugatMgr <saugatmagar433@gmail.com>
2025-02-04 15:02:29 +0545

    most borrowed books, overdue books and pending books views and urls

--- apps/books/api/v1/urls/urls.py
+++ apps/books/api/v1/urls/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 
-from apps.books.api.v1.views.dashboard.admin_dashboard import AdminDashboard
+from apps.books.api.v1.views.dashboard.admin_dashboard import (
+    AdminDashboard,
+    MostBorrowedBooks,
+    PendingBooks,
+)
 from apps.books.api.v1.views.dashboard.student import StudentDashboard
 from apps.books.api.v1.views.report import ExportDataView
 
 urlpatterns = [
     path("dashboard/admin/", AdminDashboard.as_view(), name="admin-dashboard"),
     path("dashboard/student/", StudentDashboard.as_view(), name="student-dashboard"),
-    path("export/", ExportDataView.as_view(), name="export-data"),
+    path("export-data/", ExportDataView.as_view(), name="export-data"),
+    path(
+        "most-borrowed-books/", MostBorrowedBooks.as_view(), name="most-borrowed-books"
+    ),
+    path("pending-books/", PendingBooks.as_view(), name="pending-books"),
+    path("overdue-books/", PendingBooks.as_view(), name="overdue-books"),
 ]
--- apps/books/api/v1/views/dashboard/admin_dashboard.py
+++ apps/books/api/v1/views/dashboard/admin_dashboard.py
@@ -1,18 +1,21 @@
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 
-from django.db.models import Count
+from django.db.models import Count, Exists, OuterRef
 from django.utils import timezone
 
 from apps.academic.models import Staff, Student
-from apps.books.models import Borrow
+from apps.books.api.v1.serializers.get import BookListDetailSerializer
+from apps.books.models import Book, Borrow
 from utils.constants import BorrowStatusChoices
-from utils.permissions import AdminPermission
+from utils.pagination import CustomPageSizePagination
+from utils.permissions import LibrarianOrAdminPermission
 
 
 class AdminDashboard(APIView):
     http_method_names = ["get"]
-    permission_classes = [AdminPermission]
+    permission_classes = [LibrarianOrAdminPermission]
 
     def get(self, request):
         total_students = Student.objects.count()
@@ -23,11 +26,6 @@ class AdminDashboard(APIView):
         overdue_books = Borrow.objects.filter(
             borrow_status=BorrowStatusChoices.NOT_RETURNED, due_date__lt=timezone.now()
         ).count()
-        most_borrowed_books = (
-            Borrow.objects.values("book__title")
-            .annotate(borrow_count=Count("book"))
-            .order_by("-borrow_count")
-        )
 
         return Response(
             {
@@ -35,6 +33,52 @@ class AdminDashboard(APIView):
                 "total_staff": total_staff,
                 "overdue_books": overdue_books,
                 "pending_book_returns": pending_book_returns,
-                "most_borrowed_books": most_borrowed_books,
             }
+        )
+
+
+class OverDueBooks(ListAPIView):
+    serializer_class = BookListDetailSerializer
+    permission_classes = [LibrarianOrAdminPermission]
+    pagination_class = CustomPageSizePagination
+
+    def get_queryset(self):
+        return Book.objects.filter(
+            Exists(
+                Borrow.objects.filter(
+                    book=OuterRef("id"),
+                    borrow_status=BorrowStatusChoices.NOT_RETURNED,
+                    due_date__lt=timezone.now(),
+                )
+            )
+        )
+
+
+class MostBorrowedBooks(ListAPIView):
+    serializer_class = BookListDetailSerializer
+    permission_classes = [LibrarianOrAdminPermission]
+    pagination_class = CustomPageSizePagination
+
+    def get_queryset(self):
+        queryset = (
+            Book.objects.annotate(borrow_count=Count("borrow"))
+            .filter(borrow_count__gt=0)
+            .order_by("-borrow_count")
+        )
+        return queryset
+
+
+class PendingBooks(ListAPIView):
+    serializer_class = BookListDetailSerializer
+    permission_classes = [LibrarianOrAdminPermission]
+    pagination_class = CustomPageSizePagination
+
+    def get_queryset(self):
+        return Book.objects.filter(
+            Exists(
+                Borrow.objects.filter(
+                    book=OuterRef("id"),
+                    borrow_status=BorrowStatusChoices.NOT_RETURNED,
+                )
+            )
         )
